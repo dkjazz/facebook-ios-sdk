9b590931a5b983ed5327252787ff593000d9b9a9
snorrsi <snorrsi@gmail.com>
2022-04-28 21:46:23 +0000

    Update FBSDKDynamicFrameworkLoader.m

--- FBSDKCoreKit/FBSDKCoreKit/Internal/FBSDKDynamicFrameworkLoader.m
+++ FBSDKCoreKit/FBSDKCoreKit/Internal/FBSDKDynamicFrameworkLoader.m
@@ -121,7 +121,11 @@ _fbsdkdfl_handle_get_impl_(Security)
 
 - (Class)asIdentifierManagerClass
 {
+#if FBSDK_IDFA_DISALLOWED
+  return nil;
+#else
   return fbsdkdfl_ASIdentifierManagerClass();
+#endif // FBSDK_IDFA_DISALLOWED
 }
 
 #define _fbsdkdfl_Security_get_k(SYMBOL) _fbsdkdfl_symbol_get_k(Security, SYMBOL, CFTypeRef *)
@@ -294,6 +298,8 @@ CATransform3D fbsdkdfl_CATransform3DConcat(CATransform3D a, CATransform3D b)
   return f(a, b);
 }
 
+#if !FBSDK_IDFA_DISALLOWED
+
 #pragma mark - Ad Support Classes
 
 _fbsdkdfl_load_framework_once_impl_(AdSupport)
@@ -306,6 +312,8 @@ Class fbsdkdfl_ASIdentifierManagerClass(void)
   _fbsdkdfl_AdSupport_get_c(ASIdentifierManager);
   return c;
 }
+
+#endif // FBSDK_IDFA_DISALLOWED
 
 #pragma mark - Safari Services
 _fbsdkdfl_load_framework_once_impl_(SafariServices)
